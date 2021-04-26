5e3166bed3e31fb74375faca4de211e703972bce
Martin Grønlien Pejcoch <martinp@met.no>
2021-04-26 13:42:25 +0200

    Initial Flask app draft

---
+++ dmci/api/api.py
@@ -0,0 +1,17 @@
+from flask import Flask, request
+import numpy as np
+
+def validate_mmd(data):
+    return np.random.choice([True,False])
+    
+app = Flask(__name__)
+@app.route('/', methods=['POST'])
+def base():
+    data = request.get_data()
+    if validate_mmd(data):
+        return "Nom nom I like this MMD file.", 200
+    else:
+        return "🤮", 400
+    
+app.run()
+
