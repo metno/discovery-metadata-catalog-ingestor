dd006ca01707539296ff2b1e6463fbc88ed6cce4
Morten W. Hansen <morten.wergeland.hansen@gmail.com>
2021-04-28 15:47:42 +0200

    first changes after architecture def v0

--- dmci/pycsw_dist/pycsw_dist.py
+++ dmci/pycsw_dist/pycsw_dist.py
@@ -1,24 +1,41 @@
+import logging
 import os
 import requests
 
 from py_mmd_tools.xml_utils import xml_translate
 
-from dmci.config import Config
+from dmci.distributor import Distributor, DistCmd
 
-class MMDPyCSWDist(object):
+class PyCSWDist(Distributor):
 
-    def __init__(self, mmd_file):
+    def __init__(self, cmd, xml_file=None, metadata_id=None, **kwargs):
 
-        self.conf = Config()
+        super().__init__(cmd, xml_file, metadata_identifier, **kwargs)
 
         self.mmd_file = mmd_file
 
-    def translate(self, xslt):
+    def run(self):
+
+        Distributor.run(self)
+
+        if self._cmd == distCmd.UPDATE:
+            self._update()
+        elif self._cmd == distCmd.DELETE:
+            self._delete()
+        elif self._cmd == distCmd.INSERT:
+            self._insert()
+        else:
+            logger.error('Invalid command: %s' %str(self._cmd)) # pragma: no cover
+            return False
+
+        return True
+
+    def _translate(self, xslt):
         """ Convert from MMD to ISO19139, Norwegian INSPIRE profile """
         return xml_translate(self.mmd_file, xslt)
 
-    def insert(self, xslt):
-        """ Insert in pyCSW using a Transaction operation
+    def _insert(self, xslt):
+        """ Insert in pyCSW using a Transaction 
 
         TODO:
                 - DONE: configure pycsw service (Transactions are disabled by default; to enable, `manager.transactions` must be set to true. Access to transactional functionality is limited to IP addresses which must be set in `manager.allowed_ips`.)
@@ -34,16 +51,17 @@ class MMDPyCSWDist(object):
         header = """<?xml version="1.0" encoding="UTF-8"?><csw:Transaction xmlns:csw="http://www.opengis.net/cat/csw/2.0.2" xmlns:ows="http://www.opengis.net/ows" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.opengis.net/cat/csw/2.0.2 http://schemas.opengis.net/csw/2.0.2/CSW-publication.xsd" service="CSW" version="2.0.2"><csw:Insert>"""
         footer = """</csw:Insert></csw:Transaction>"""
         xml_as_string = header + iso + footer
-        return requests.post(self.conf.csw_service_url, headers=headers, data=xml_as_string)
+        return requests.post(self._conf.csw_service_url, headers=headers, data=xml_as_string)
 
-    def update(self):
+    def _update(self):
         """ Update current entry
 
         Need to find out how to do this...
         """
-        pass
+        logger.error("Not yet implemented")
+        return False
 
-    def delete(self, id):
+    def _delete(self, id):
 
         headers = requests.structures.CaseInsensitiveDict()
         headers["Content-Type"] = "application/xml"
@@ -61,4 +79,4 @@ class MMDPyCSWDist(object):
             </csw:Constraint>
           </csw:Delete>
         </csw:Transaction>""" %id
-        return requests.post(self.conf.csw_service_url, headers=headers, data=xml_as_string)
+        return requests.post(self._conf.csw_service_url, headers=headers, data=xml_as_string)
