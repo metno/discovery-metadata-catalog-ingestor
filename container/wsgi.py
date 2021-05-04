00029c3f7645d4f76f8b297485d92dab4c78c318
Veronica Berglyd Olsen <1619840+vkbo@users.noreply.github.com>
2021-05-04 15:50:39 +0200

    Update the wsgi script

--- container/wsgi.py
+++ container/wsgi.py
@@ -21,15 +21,10 @@ limitations under the License.
 import os
 import sys
 
-# Note: This line forces the test suite to import the dmci package in the current source tree
-sys.path.insert(
-    1, os.path.abspath(os.path.join(os.path.dirname(__file__),
-                                    os.path.pardir)))
-
 from dmci.api import App
 from dmci import CONFIG
 
-CONFIG.readConfig()
+if not CONFIG.readConfig(configFile=os.environ.get("DMCI_CONFIG", None)):
+    sys.exit(1)
 
-app = App()._app
-# app._app.run()%     
+app = App()
