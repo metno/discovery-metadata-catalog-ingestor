708b6bd7339e6f55a581c3772d2f5db7e465b727
Johannes Langvatn <johannestl@met.no>
2023-08-17 15:39:18 +0200

    Return message if empty post

--- dmci/api/app.py
+++ dmci/api/app.py
@@ -117,6 +117,8 @@ class App(Flask):
 
     def _insert_update_method_post(self, cmd, request):
         """Process insert or update command requests."""
+        if request.content_length is None:
+            return "There is no data sent to the api", 202
         if request.content_length > self._conf.max_permitted_size:
             return f"The file is larger than maximum size: {self._conf.max_permitted_size}", 413
 
